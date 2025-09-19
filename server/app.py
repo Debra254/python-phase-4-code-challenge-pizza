06856065602161d1b7d5527be55c4356134eaa8e
Debra <debrawangeci@gmail.com>
2025-09-20 00:25:27 +0300

    add all changes

--- server/app.py
+++ server/app.py
@@ -25,5 +25,53 @@ def index():
     return "<h1>Code challenge</h1>"
 
 
+@app.route('/restaurants')
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return [restaurant.to_dict(only=('id', 'name', 'address')) for restaurant in restaurants]
+
+
+@app.route('/restaurants/<int:id>')
+def get_restaurant(id):
+    restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+    if restaurant:
+        return restaurant.to_dict(only=('id', 'name', 'address', 'restaurant_pizzas.id', 'restaurant_pizzas.price', 'restaurant_pizzas.pizza_id', 'restaurant_pizzas.restaurant_id', 'restaurant_pizzas.pizza.id', 'restaurant_pizzas.pizza.name', 'restaurant_pizzas.pizza.ingredients'))
+    else:
+        return {"error": "Restaurant not found"}, 404
+
+
+@app.route('/restaurants/<int:id>', methods=['DELETE'])
+def delete_restaurant(id):
+    restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+    if restaurant:
+        db.session.delete(restaurant)
+        db.session.commit()
+        return '', 204
+    else:
+        return {"error": "Restaurant not found"}, 404
+
+
+@app.route('/pizzas')
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return [pizza.to_dict(only=('id', 'name', 'ingredients')) for pizza in pizzas]
+
+
+@app.route('/restaurant_pizzas', methods=['POST'])
+def create_restaurant_pizza():
+    data = request.get_json()
+    try:
+        restaurant_pizza = RestaurantPizza(
+            price=data['price'],
+            pizza_id=data['pizza_id'],
+            restaurant_id=data['restaurant_id']
+        )
+        db.session.add(restaurant_pizza)
+        db.session.commit()
+        return restaurant_pizza.to_dict(only=('id', 'price', 'pizza_id', 'restaurant_id', 'pizza.id', 'pizza.name', 'pizza.ingredients', 'restaurant.id', 'restaurant.name', 'restaurant.address')), 201
+    except ValueError as e:
+        return {"errors": ["validation errors"]}, 400
+
+
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
